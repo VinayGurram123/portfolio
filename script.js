2217652c94495c4301e1b722ca2e33dda71ad4db
VinayGurram123 <vinay.gurram0655@gmail.com>
2024-09-08 16:19:30 +0530

    chnge

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
-// const hamburger = document.getElementById('hamburger');
-// const navlink = document.getElementById('nav-list-links');
-
-// hamburger.addEventListener('click', () => {
-   
-//     navlink.classList.toggle('active');
-//     hamburger.classList.toggle('active');
-// });
-
-
 const hamburger = document.getElementById('hamburger');
-const responsiveNav = document.querySelector('.responsive-nav');
+const navlink = document.getElementById('nav-list-links');
 
 hamburger.addEventListener('click', () => {
-    // Toggle the 'active' class for both hamburger and responsive nav
-    responsiveNav.classList.toggle('active');
+   
+    navlink.classList.toggle('active');
     hamburger.classList.toggle('active');
-    
-    // If you want to hide the nav-list-links on mobile, you can keep it as display none
-    // document.getElementById('nav-list-links').style.display = 'none';
 });
